2e4a6cdac4f661fc35e9ca3e77cc454a605133ce
RomanKotyubeev <45431768+RomanKotybeev@users.noreply.github.com>
2019-09-28 22:45:54 +0700

    Update TrafficModel.java

--- src/solvedbyobjects/TrafficModel.java
+++ src/solvedbyobjects/TrafficModel.java
@@ -8,27 +8,27 @@ import processing.core.PApplet;
  * Date: March 17, 2019
  */
 
-//To display cars was used processing library
-//It needs to inherit PApplet
+// To display cars was used processing library
+// It needs to inherit PApplet
 public class TrafficModel extends PApplet {
 	
-	//classes responsible for creating horizontal and vertical (X and Y) roads with cars 
+	// classes responsible for creating horizontal and vertical (X and Y) roads with cars 
 	Road roadX;
 	Road roadY;
 	
-	//in processing 3 needs to be in the main class. 
-	//Another way is to add an argument "package.class" in the run configuration 
+	// in processing 3 needs to be in the main class. 
+	// Another way is to add an argument "package.class" in the run configuration 
 	public static void main(String[] args) {
         PApplet.main("solvedbyobjects.TrafficModel");
-    }
+    	}
 	
-	//It needs to be here in new version of processing. 
-	//The size method must be here not in the setup method 
+	// It needs to be here in new version of processing. 
+	// The size method must be here not in the setup method 
 	public void settings() {
 		size(640, 640);
 	}
 	
-	//setup method will be initialized just one time
+	// setup method will be initialized just one time
 	public void setup() {
 		//background is white
 		background(255);
@@ -38,7 +38,7 @@ public class TrafficModel extends PApplet {
 		roadY = new RoadY(this);
 	}
 
-	//draw method will be always run until you don't stop by hand
+	// draw method will be always run until you don't stop by hand
 	public void draw() {
 		roadX.run(this);
 		roadY.run(this);
